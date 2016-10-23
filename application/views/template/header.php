1414db50e39b210346bfd34b42c02877fd840517
refandhika <refandika@gmail.com>
2016-10-23 22:50:33 +0700

    Add Initial Files

---
+++ application/views/template/header.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php echo $title; ?></title>
+	<!-- Style -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/normalize.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<!--<link rel="stylesheet" href="assets/css/style-s.css">-->
+</head>
+
+<body>
+	<!-- Header Bar -->
